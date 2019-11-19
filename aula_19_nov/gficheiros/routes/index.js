f1c7e3463b2635fcd69c97cb3e58fa7cd5686a46
Costeira <joaocurveira13@gmail.com>
2019-11-19 09:31:42 +0000

    up

--- aula_19_nov/gficheiros/routes/index.js
+++ aula_19_nov/gficheiros/routes/index.js
@@ -1,9 +1,26 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
+const axios = require('axios')
+const lhost = require('../config/env').host
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+
+
+router.get('/',function(req,res){
+  axios.get(host + 'api/ficheiros')
+    .then(dados=>{
+
+      res.render('index',{lista: dados.data});
+    })
+    .catch(erro =>{
+        res.render('erro',{error:erro})
+    })
+})
+
+
+router.get('dowload/:fnome',function(req,res){
+
+    res.download(__dirname + '/../public/ficheiros/' + req.params.fnome)
+})
 
 module.exports = router;
+
