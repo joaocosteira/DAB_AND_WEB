65f53b1d16229edc3487d0bb4279c720899129b8
Costeira <joaocurveira13@gmail.com>
2019-11-19 09:35:30 +0000

    pu

---
+++ aula_19_nov/gficheiros/models/ficheiro.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+var ficheiroSchema = new mongoose.Schema({
+  
+    data: String,
+    desc: String,
+    name: String,
+    mimetype: String,
+    size: Number,
+
+})
+
+module.exports = mongoose.model('ficheiro', ficheiroSchema)
